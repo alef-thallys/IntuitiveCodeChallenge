8b1c5439ec6dce06e99a0fe80e5561490c1e9edf
alef-thallys <alef.thallys22@gmail.com>
2025-03-27 01:12:21 -0300

    feat: move expense queries to a new analytics_query.sql file and remove from scripts.sql

--- scripts.sql
+++ scripts.sql
@@ -192,39 +192,3 @@ SET
     descricao = TRIM(NULLIF(@descricao, '')),  
     vl_saldo_inicial = REPLACE(NULLIF(@vl_saldo_inicial, ''), ',', '.'),
     vl_saldo_final = REPLACE(NULLIF(@vl_saldo_final, ''), ',', '.');
-
-
--- Consulta para obter as 10 operadoras com as maiores despesas no último trimestre
-SELECT 
-    o.razao_social, 
-    SUM(d.vl_saldo_final) AS total_despesas
-FROM 
-    demonstracoes_contabeis d
-JOIN 
-    operadoras_ativas o ON d.reg_ans = o.registro_ans
-WHERE 
-    REGEXP_REPLACE(d.descricao, '\\s+', ' ') LIKE '%EVENTOS/ SINISTROS CONHECIDOS OU AVISADOS DE ASSISTÊNCIA A SAÚDE MÉDICO HOSPITALAR%'
-    AND d.data = '2024-10-01'
-GROUP BY 
-    o.razao_social
-ORDER BY 
-    total_despesas DESC
-LIMIT 10;
-
-
--- Consulta para obter as 10 operadoras com as maiores despesas no último ano
-SELECT 
-    d.data,
-    o.razao_social, 
-    SUM(d.vl_saldo_final) AS total_despesas
-FROM 
-    demonstracoes_contabeis d
-JOIN 
-    operadoras_ativas o ON d.reg_ans = o.registro_ans
-WHERE 
-     d.data >= DATE_SUB(CURDATE(), INTERVAL 1 YEAR)
-GROUP BY 
-    o.razao_social
-ORDER BY 
-    total_despesas DESC
-LIMIT 10;
